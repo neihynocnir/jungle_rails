7b4a7eba19495bb9163539195d684838ababba6d
Neihy Rincon <neihy.rincon@gmail.com>
2020-06-03 17:35:16 +0000

    finishing model spec user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates_uniqueness_of :email
   validates_length_of :password, minimum: 8, on: :create
+
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
